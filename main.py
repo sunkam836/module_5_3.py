e352db95c35b679e7e9247773f44894315380c06
sunkam836 <sunkam836@gmail.com>
2024-12-29 22:34:09 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return NotImplemented
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+        elif isinstance(value, House):
+            return House(self.name, self.number_of_floors + value.number_of_floors)
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+    def __str__(self):
+            return (f'Название: "{self.name}", '
+                    f'кол-во этажей: {self.number_of_floors}')
+
+
+h1 = House('Алмаз', 3)
+h2 = House('Ривьера', 19)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 5 # __iadd__
+print(h1)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
